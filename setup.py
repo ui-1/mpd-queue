c4cc1babd5b837187c1be957a82906aa0762b567
ui-1 <ui-1@posteo.org>
2023-06-11 09:53:01 +0300

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mpd-queue',
+    version='1.0',
+    packages=['beetsplug'],
+    url='https://github.com/ui-1/mpd-queue',
+    license='GPLv3',
+    author='ui-1',
+    author_email='ui-1@posteo.org',
+    description='Add all imported tracks to an MPD server\'s queue'
+)
